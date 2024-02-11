9afa78c0d61e443351e5ca9905d6c528d652e5cb
Shubham Singh <36928095+shubhams167@users.noreply.github.com>
2024-02-11 16:13:25 +0530

    conditionally render routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import { ReactFlowProvider } from "reactflow";
-import { Home, Create, View, Settings } from "./routes";
-import {
-  // createBrowserRouter,
-  createHashRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import { Layout } from "./components/layout";
+import { router } from "./routes";
+import { RouterProvider } from "react-router-dom";
 
 export default function App() {
-  const router = createHashRouter(
-    createRoutesFromElements(
-      <Route element={<Layout />}>
-        <Route path="/" index element={<Home />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/view" element={<View />} />
-        <Route path="/settings" element={<Settings />} />
-      </Route>
-    )
-  );
-
   return (
     <ReactFlowProvider>
       <RouterProvider router={router} />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,30 @@
+import {
+  Route,
+  createBrowserRouter,
+  createHashRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import { Layout } from "../components/layout";
+import { Home } from "./home";
+import { Create } from "./create";
+import { View } from "./view";
+import { Settings } from "./settings";
+
 export * from "./home";
 export * from "./create";
 export * from "./view";
 export * from "./settings";
+
+const routes = createRoutesFromElements(
+  <Route element={<Layout />}>
+    <Route path="/" index element={<Home />} />
+    <Route path="/create" element={<Create />} />
+    <Route path="/view" element={<View />} />
+    <Route path="/settings" element={<Settings />} />
+  </Route>
+);
+
+export const router =
+  process.env.REACT_APP_USE_HASH_ROUTER === "true"
+    ? createHashRouter(routes)
+    : createBrowserRouter(routes);
